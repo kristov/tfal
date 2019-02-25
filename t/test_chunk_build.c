c3e9da6555a8594c3d6b1952acad3fe3ce4cea85
ceade <chris.eade@gmail.com>
2019-02-25 08:24:52 +0100

    Test for building data chunks

--- t/test_chunk_build.c
+++ t/test_chunk_build.c
@@ -44,36 +44,41 @@ uint8_t TEST_STRUCTURE[] = {
 
 void test_build_replica(test_harness_t* test) {
     uint8_t* data = malloc(sizeof(uint8_t) * 36);
-    uint64_t addr = 0;
+    uint64_t addr1 = 0;
+    uint64_t addr2 = 0;
     chunk_t work;
 
     chunk_write_header(data, CHUNK_TYPE_SET, 27);
     work = chunk_decode(data);
 
-    addr = chunk_set_item_byte_offset(work, 0);
-    chunk_write_header(&data[addr], CHUNK_TYPE_UINT32, 1);
+    addr1 = chunk_set_item_byte_offset(work, 0);
+    is_equal_uint64(test, addr1, 9, "test_build_replica(): first level set offset 0 correct");
+    chunk_write_header(&data[addr1], CHUNK_TYPE_UINT32, 1);
 
-    addr = chunk_set_item_byte_offset(work, 1);
-    chunk_write_header(&data[addr], CHUNK_TYPE_SET, 9);
+    addr1 = chunk_set_item_byte_offset(work, 1);
+    is_equal_uint64(test, addr1, 12, "test_build_replica(): first level set offset 1 correct");
+    chunk_write_header(&data[addr1], CHUNK_TYPE_SET, 9);
+    work = chunk_decode(&data[addr1]);
+    *work.data = 9;
 
-    work = chunk_decode(&data[addr]);
+    work = chunk_decode(&data[addr1]);
 
-    addr = chunk_set_item_byte_offset(work, 0);
-    chunk_write_header(&data[addr], CHUNK_TYPE_UINT32, 1);
+    addr2 = chunk_set_item_byte_offset(work, 0);
+    chunk_write_header(&data[addr1 + addr2], CHUNK_TYPE_UINT32, 1);
 
-    addr = chunk_set_item_byte_offset(work, 1);
-    chunk_write_header(&data[addr], CHUNK_TYPE_UINT8, 1);
+    addr2 = chunk_set_item_byte_offset(work, 1);
+    chunk_write_header(&data[addr1 + addr2], CHUNK_TYPE_UINT8, 1);
 
-    addr = chunk_set_item_byte_offset(work, 2);
-    chunk_write_header(&data[addr], CHUNK_TYPE_INT8, 1);
+    addr2 = chunk_set_item_byte_offset(work, 2);
+    chunk_write_header(&data[addr1 + addr2], CHUNK_TYPE_INT8, 1);
 
     work = chunk_decode(data);
 
-    addr = chunk_set_item_byte_offset(work, 2);
-    chunk_write_header(&data[addr], CHUNK_TYPE_UINT16, 1);
+    addr1 = chunk_set_item_byte_offset(work, 2);
+    chunk_write_header(&data[addr1], CHUNK_TYPE_UINT16, 1);
 
-    addr = chunk_set_item_byte_offset(work, 3);
-    chunk_write_header(&data[addr], CHUNK_TYPE_INT8, 1);
+    addr1 = chunk_set_item_byte_offset(work, 3);
+    chunk_write_header(&data[addr1], CHUNK_TYPE_INT8, 1);
 
     for (uint8_t i = 0; i < 36; i++) {
         is_equal_uint8(test, data[i], TEST_STRUCTURE[i], "test_build_replica(): iter");
