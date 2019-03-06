7e9f3f6f4ac5b14686b954af1d11c3124fdecdc4
ceade <chris.eade@gmail.com>
2019-03-06 22:42:19 +0100

    Basic traversing of a tree

--- curses.c
+++ curses.c
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <fcntl.h>
@@ -11,45 +12,13 @@
 #define CHUNK_COLOR_SET 1
 #define CHUNK_COLOR_ITEM 2
 
-//                   __ Item index 1 of the set, which is index 2 of the
-//                  /   root set.
-//                 /
-// S[I..I...S[I...I....I]]
-//
-// Keypad left does:
-//
-//   Decrements the cursor_path_idx. Regenerates cursor_byte_offset from the
-//   path.
-//
-// Keypad right does:
-//
-//   If the current object is a set:
-//     Increments cursor_path_idx and sets cursor_path[cursor_path_idx] to 0 if
-//     the current object. Regenerates cursor_byte_offset.
-//   If the current object is an item:
-//     Nothing yet, but could browse through data
-//
-// Keypad up:
-//   If cursor_path[cursor_path_idx-1] is a set:
-//     Decrement cursor_path[cursor_path_idx]. Do not decrement past zero.
-//     Regenerate cursor_byte_offset.
-//
-// Keypad down:
-//
-//   If cursor_path[cursor_path_idx-1] is a set:
-//     Increment cursor_path[cursor_path_idx]. Check parent set for end of set
-//     index condition. Regenerate cursor_byte_offset.
-//
-// When rendering:
-//   If cursor_byte_offset is equal to the start of the current chunk
-//   highlight it.
-//
 typedef struct c_context {
     int fd;
     uint8_t* start;
     uint32_t cursor_path[256];
     uint8_t cursor_path_idx;
     uint64_t cursor_byte_offset;
+    uint64_t current_byte_offset;
 } c_context_t;
 
 // uint64_t chunk_byte_offset(uint8_t* data, uint32_t* idx, uint32_t nr_idx) {
@@ -81,21 +50,25 @@ void draw_box(uint8_t xoff, uint8_t yoff, uint8_t w, uint8_t h) {
 }
 
 void draw_set(c_context_t* context, chunk_t chunk, uint8_t xoff, uint8_t yoff) {
-    attron(COLOR_PAIR(CHUNK_COLOR_SET));
-    draw_box(xoff, yoff, 10, 1);
+    uint8_t highlight = 0;
+    if (context->current_byte_offset == context->cursor_byte_offset) {
+        highlight = 2;
+    }
+    attron(COLOR_PAIR(CHUNK_COLOR_SET + highlight));
+    draw_box(xoff, yoff, 2, 1);
     mvprintw(yoff, xoff, "%s", name_per_type[chunk.type]);
-    attroff(COLOR_PAIR(CHUNK_COLOR_SET));
+    attroff(COLOR_PAIR(CHUNK_COLOR_SET + highlight));
 }
 
 void draw_item(c_context_t* context, chunk_t chunk, uint8_t xoff, uint8_t yoff) {
     uint8_t highlight = 0;
-    //if (context->set_index == context->cursor_set_index && context->current_depth == context->cursor_depth) {
-    //    highlight = 2;
-    //}
+    if (context->current_byte_offset == context->cursor_byte_offset) {
+        highlight = 2;
+    }
     uint8_t bytes_per_type = chunk_bytes_per_type(chunk.type);
     uint64_t nr_items = chunk.data_length / bytes_per_type;
     attron(COLOR_PAIR(CHUNK_COLOR_ITEM + highlight));
-    draw_box(xoff, yoff, 10, 1);
+    draw_box(xoff, yoff, 3, 1);
     mvprintw(yoff, xoff, "%s:%lu", name_per_type[chunk.type], nr_items);
     attroff(COLOR_PAIR(CHUNK_COLOR_ITEM + highlight));
 }
@@ -106,20 +79,18 @@ uint8_t draw_chunk(c_context_t* context, chunk_t chunk, uint8_t xoff, uint8_t yo
         uint8_t this_height = 1;
         uint64_t remaining = chunk.data_length;
         uint8_t* data = chunk.data;
-        //context->set_index = 0;
+        context->current_byte_offset += (1 + chunk.nr_length_bytes);
         while (remaining) {
             chunk_t child = chunk_decode(data);
-            //context->current_depth++;
             this_height += draw_chunk(context, child, xoff + 1, yoff + this_height);
-            //context->current_depth--;
             data = data + child.total_length;
             remaining = remaining - child.total_length;
-            //context->set_index++;
         }
         return this_height;
     }
 
     draw_item(context, chunk, xoff, yoff);
+    context->current_byte_offset += chunk.total_length;
     return 1;
 }
 
@@ -158,6 +129,53 @@ void load_file(c_context_t* context, const char* file) {
     return;
 }
 
+void regenerate_cursor_offset(c_context_t* context) {
+    context->cursor_byte_offset = chunk_byte_offset(context->start, context->cursor_path, (uint32_t)context->cursor_path_idx + 1);
+    mvprintw(0, 0, "%lu   ", context->cursor_byte_offset);
+}
+
+uint8_t key_up(c_context_t* context) {
+    if (context->cursor_path[context->cursor_path_idx] == 0) {
+        return 0;
+    }
+    context->cursor_path[context->cursor_path_idx]--;
+    regenerate_cursor_offset(context);
+    return 1;
+}
+
+uint8_t key_down(c_context_t* context) {
+    context->cursor_path[context->cursor_path_idx]++;
+    regenerate_cursor_offset(context);
+    // As I don't have an indication that the index exceeds the length of the
+    // set, looking for a zero here is an expensive way of backing off the end
+    // of the set.
+    if (context->cursor_byte_offset == 0) {
+        context->cursor_path[context->cursor_path_idx]--;
+        regenerate_cursor_offset(context);
+    }
+    return 1;
+}
+
+uint8_t key_left(c_context_t* context) {
+    if (context->cursor_path_idx == 0) {
+        return 0;
+    }
+    context->cursor_path_idx--;
+    regenerate_cursor_offset(context);
+    return 1;
+}
+
+uint8_t key_right(c_context_t* context) {
+    chunk_t chunk = chunk_decode(context->start + context->cursor_byte_offset);
+    if (chunk.type == CHUNK_TYPE_SET) {
+        context->cursor_path_idx++;
+        context->cursor_path[context->cursor_path_idx] = 0;
+        regenerate_cursor_offset(context);
+        return 1;
+    }
+    return 0;
+}
+
 void loop(c_context_t* context) {
     uint8_t running = 1;
     uint8_t render = 1;
@@ -165,16 +183,23 @@ void loop(c_context_t* context) {
         int c = getch();
         switch (c) {
             case KEY_UP:
-                render = 1;
+                render = key_up(context);
+                break;
+            case KEY_DOWN:
+                render = key_down(context);
+                break;
+            case KEY_LEFT:
+                render = key_left(context);
                 break;
             case KEY_RIGHT:
-                render = 1;
+                render = key_right(context);
                 break;
             default:
                 break;
         }
         if (render) {
             chunk_t chunk = chunk_decode(context->start);
+            context->current_byte_offset = 0;
             draw_chunk(context, chunk, 1, 1);
             refresh();
             render = 0;
@@ -184,10 +209,10 @@ void loop(c_context_t* context) {
 
 void init_context(c_context_t* context) {
     context->fd = -1;
-    //context->cursor_depth = 1;
-    //context->cursor_set_index = 0;
-    //context->set_index = 0;
-    //context->current_depth = 0;
+    memset(context->cursor_path, 0, 256);
+    context->cursor_path_idx = 0;
+    context->cursor_byte_offset = 0;
+    context->current_byte_offset = 0;
 }
 
 void initcolors() {
