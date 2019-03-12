9d308991f27db1752172efee5838b2fb28990843
ceade <chris.eade@gmail.com>
2019-03-12 17:48:34 +0100

    Nothing commit

--- chunk_node.h
+++ chunk_node.h
@@ -1,5 +1,5 @@
-#ifndef H_AST
-#define H_AST
+#ifndef H_CHUNK_NODE
+#define H_CHUNK_NODE
 
 #include <stdint.h>
 #include "chunk.h"
@@ -8,12 +8,12 @@ typedef struct chunk_node chunk_node_t;
 
 typedef struct chunk_node {
     chunk_type_t type;
-    uint64_t nr_children;
-    chunk_node_t* children;
-    uint8_t bytes_per_type;
     uint8_t selected;
+    uint8_t bytes_per_type;
     uint8_t* address;
     uint8_t* data;
+    uint64_t nr_children;
+    chunk_node_t* children;
 } chunk_node_t;
 
 chunk_node_t* chunk_node_select(chunk_node_t* node, uint64_t* addr, uint64_t nr_addr);
